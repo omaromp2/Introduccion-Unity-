c042b89151efac8053c7bdf0480197b104edb21a
Omar Ponce <omargponce@hotmail.com>
2015-01-23 18:20:43 -0400

    Intoduccion a Unity

---
+++ Assets/Scripts/ControlCamara.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlCamara : MonoBehaviour {
+	public GameObject Jugador; 
+	private Vector3 offset; 
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position;	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.position = Jugador.transform.position + offset; 	
+	}
+}
---
+++ Assets/Scripts/ControlJugador.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class ControlJugador : MonoBehaviour 
+{
+	public float speed;
+	private int count; 
+	public Text countText;
+	public Text winText; 
+
+	void Start()
+	{
+		count = 0; // inicializacion de la puntuacion
+		SetCountText();
+		winText.text = ""; 
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");	
+		float moveVertical   = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1 ;
+			SetCountText(); 
+		}
+	}
+
+	//creamos funcion para count text 
+	void SetCountText()
+	{
+		countText.text = "Puntuacion: " + count.ToString();
+		if(count >= 7)
+		{
+			winText.text = "Gane el Juego! :D";
+		}
+	}
+}
